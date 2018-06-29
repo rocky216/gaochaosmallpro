8026d7b1efce66693031976790dd1012f254a341
rocky216 <1120247491@qq.com>
2018-06-29 17:14:46 +0800

    ds

--- pages/clock/clock.js
+++ pages/clock/clock.js
@@ -5,25 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    markers: [{
+      iconPath: "/images/clock.png",
+      id: 0,
+      latitude: 27.12,
+      longitude: 114.8977,
+      width: 50,
+      height: 50
+    }],
   }, 
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // wx.getLocation({
-    //   success: function(res) {
+    
+    // wx.chooseLocation({
+    //   success: function (res) {
     //     console.log(res)
     //   },
     // })
-    wx.chooseLocation({
-      success: function(res) {
+    
+  },
+  clock: function(){
+    wx.getLocation({
+      type: "wgs84",
+      success: function(res){
         console.log(res)
-      },
+
+      }
     })
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -35,9 +48,29 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    let len = this.getDistance("27.12", "114.8977", "27.12005", "114.89775")
+    console.log(len)
   },
+  getDistance: function (lat1, lng1, lat2, lng2) {
+    lat1 = lat1 || 0;
+    lng1 = lng1 || 0;
+    lat2 = lat2 || 0;
+    lng2 = lng2 || 0;
 
+    var rad1 = lat1 * Math.PI / 180.0;
+    var rad2 = lat2 * Math.PI / 180.0;
+    var a = rad1 - rad2;
+    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+
+    var r = 6378137;
+    var distance = r * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(rad1) * Math.cos(rad2) * Math.pow(Math.sin(b / 2), 2)));
+
+    /*if (distance > 1000){
+      distance = Math.round(distance / 1000);
+    }*/
+
+    return distance;
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
